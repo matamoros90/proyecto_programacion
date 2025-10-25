c38997fd92e885580346f3234818d0705714a822
Co-Matamoros <136269102+matamoros90@users.noreply.github.com>
2025-10-24 19:18:33 -0600

    Update fisica.router.js

--- src/routes/fisica.router.js
+++ src/routes/fisica.router.js
@@ -3,17 +3,14 @@ const router = express.Router();
 const controller = require('../controllers/fisica.controller');
 
 router.get('/create', controller.create);
-
 router.post('/', controller.store);
-
 router.get('/', controller.index);
 
-router.get('/:id', controller.show);
-
 router.get('/:id/edit', controller.edit);
-
 router.put('/:id', controller.update);
 
+router.get('/:id', controller.show);
 router.delete('/:id', controller.destroy);
 
 module.exports = router;
+
